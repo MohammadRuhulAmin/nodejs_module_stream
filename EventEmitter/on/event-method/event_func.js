23bce73b4b3ef92422911f9f1bead00bd2a6708f
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-12-12 11:41:30 +0600

    buffer directory added

--- EventEmitter/on/event-method/event_func.js
+++
@@ -1,9 +0,0 @@
-import {EventEmitter} from "node:events"
-class MyEmitter extends EventEmitter{}
-const myEmitter  = new MyEmitter()
-/**Dynamic (bound to myEmitter) */
-myEmitter.on('event',function(a,b){
-    console.log(a,b,this,this === myEmitter)
-})
-
-myEmitter.emit('event')
