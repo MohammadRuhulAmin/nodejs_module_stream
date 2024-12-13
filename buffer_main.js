7e056c95cfed63cf61891e8a6d03844715e8b114
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-12-13 13:12:29 +0600

    buffer main.js added

---
+++ buffer_main.js
@@ -0,0 +1,34 @@
+import {Buffer,constants} from "node:buffer"
+
+/**
+ * By allocating the size 4 of the buffer will be 4 byte or 4*8 = 32 bits
+ * In each index there are 8 bits to be allocated
+ * 
+ */
+const memoryContainer = Buffer.alloc(4) /** allocate 4 bytes */
+console.log(memoryContainer) /** print <Buffer 00 00 00 00> this represents hexadecimal format  */
+
+/** In each index minimum value 0 and maximum 255 in decimal representation can hold */
+memoryContainer[0] = 0xF4
+memoryContainer[1] = 0xFf
+memoryContainer[2] = 0x00
+// memoryContainer.writeInt8(-34,2) /** for writing negative integer and the offset is the second parameter */
+memoryContainer[3] = 0xff
+
+console.log(memoryContainer[0])
+console.log(memoryContainer[1])
+console.log(memoryContainer.readInt8(2)) /** To print the negative integer of index 2 in the buffer */
+console.log(memoryContainer[3])
+
+console.log(memoryContainer.toString('hex'))
+
+/** from method will automatically allocate bytes */
+const buff = Buffer.from([0x48,0x69,0x21])
+console.log(buff.toString())
+/**https://symbl.cc */
+const buff2 = Buffer.from("E29E9C","hex") 
+console.log(buff2.toString("utf-8"))
+
+const buff3 = Buffer.from("E29FBE","hex")
+console.log(buff3.toString())
+console.log(constants.MAX_LENGTH) /**4294967296 bytes or 4GB */
