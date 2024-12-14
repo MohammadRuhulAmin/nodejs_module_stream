13d78459841970c95f78ba46d03632c25259140c
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-12-14 20:14:03 +0600

    create file command API added

--- project/app.js
+++ project/app.js
@@ -9,25 +9,48 @@ import * as fsPromise from "node:fs/promises"
  */
 
 (async ()=>{
+    /** commands */
+    const CREATE_FILE = "create a file"
+    
+    const createFile = async(path)=>{
+        let existingFileHandle,newFileHandle;
+        try{
+            existingFileHandle = await fsPromise.open(path,'r')
+            console.log(`The file ${path} already exist.`)
+        }catch(error){
+            newFileHandle = await fsPromise.open(path,"w")
+            console.log(`${path} file has been successfully created.`)
+        }finally{
+            existingFileHandle?.close()
+            newFileHandle?.close()
+        }
+
+    }
+    
     const commandFilehandler = await fsPromise.open("./files/command.txt",'r') 
     commandFilehandler.on('change',async()=>{
-        
-        console.log('file changed!')
         /*
         const content = await commandFilehandler.read() // here the default buffer with a big allocation 
         will hold data.
         */
+
        /** get the size of our file */
         const size = (await commandFilehandler.stat()).size
         /** allocate our buffer with the size of the file */
         const buffer = Buffer.alloc(size)
         /** the location at which we want to start filling our buffer */
         const offset = 0
-        const position =0 /** position of the string index in the file */
+        const position = 0 /** position of the string index in the file */
         /** how many bytes we ant to read */
         const length = buffer.byteLength
         await commandFilehandler.read(buffer,offset,length,position)
-
+        console.log(buffer.toString('utf-8'))
+        const command = buffer.toString()
+        if(command.includes(CREATE_FILE)){
+            
+            const filePath = command.substring(CREATE_FILE.length + 1)
+            createFile(filePath)
+        }
 
         
     })
